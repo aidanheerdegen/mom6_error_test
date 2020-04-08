c9c60c97b1b750cb5d492a5b68e798349f73fa4b
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2020-04-08 11:54:07 +1000

    Added resubmit script

---
+++ resub.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+logfile='resubmit.log'
+counterfile='resubmit.count'
+
+
+# Define errors from which a resubmit is appropriate
+declare -a errors={"DT found with multiple inconsistent definitions", 
+                   "Segmentation fault: address not mapped to object"}
+
+resub=false
+for error in "${errors[@]}"
+do
+  if grep -q error ${outfile}
+  then
+     resub=true
+     break
+  fi
+done
+
+if ! resub
+then
+  echo "Error no eligible for resubmission" >> ${logfile}
+  exit 0
+fi
+
+if [ -f "${counterfile}" ]
+then
+  PAYU_N_RESUB=$(cat ${counterfile})
+else
+  echo "Reset resubmission counter" >> ${logfile}
+  PAYU_N_RESUB=3
+fi
+
+echo "Resubmission counter: ${PAYU_N_RESUB}" >> ${logfile}
+
+if [[ "${PAYU_N_RESUB}" -gt 0 ]]
+then
+  # Sweep and re-run
+  ${PAYU_PATH}/payu sweep >> ${logfile}
+  ${PAYU_PATH}/payu run -n ${PAYU_N_RUNS} >> ${logfile}
+  # Decrement resub counter and save to counter file
+  ((PAYU_N_RESUB=PAYU_N_RESUB-1)) 
+  echo "${PAYU_N_RESUB}" > ${counterfile}
+else
+  echo "Resubmit limit reached ... " >> ${logfile}
+  rm ${counterfile}
+fi
+
